e593b9e3790b745411d26daf34a08260b6611a04
benixal <behnamxben@gmail.com>
2022-12-13 05:22:06 +0330

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SayHi name="ben"></SayHi>
+  
+</template>
+
+<script>
+import SayHi from './components/SayHi.vue'
+export default {
+  name: 'App',
+  components: {
+    SayHi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SayHi.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="sayhi">click here {{name}}</button>
+    </div>
+    <div>{{himsg}}</div>
+</template>
+
+<script>
+export default {
+    name:"SayHi",
+    props: {
+        name:String
+    },
+    data:()=> {
+        return {
+            himsg:'welcome'
+        }
+    },
+    methods: {
+        sayhi:function() {
+            this.himsg = "Hi "+this.name;
+        }
+    },
+}
+</script>
